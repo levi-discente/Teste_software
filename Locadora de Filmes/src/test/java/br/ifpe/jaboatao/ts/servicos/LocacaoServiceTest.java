4446b5f6a89964a9f365e9d6d72f58b3513d44dc
Levi-discente <lrsa1@discente.ifpe.edu.br>
2024-10-07 21:50:32 -0300

    fix maven

--- Locadora de Filmes/src/test/java/br/ifpe/jaboatao/ts/servicos/LocacaoServiceTest.java
+++ Locadora de Filmes/src/test/java/br/ifpe/jaboatao/ts/servicos/LocacaoServiceTest.java
@@ -1,7 +1,5 @@
 package br.ifpe.jaboatao.ts.servicos;
 
-
-
 import java.util.Date;
 
 import org.junit.jupiter.api.Assertions;
@@ -15,21 +13,21 @@ import br.ifpe.jaboatao.ts.utils.DataUtils;
 
 public class LocacaoServiceTest {
 
-	@Test
-	public void teste01(){
-		//Cenário
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuário 01");
-		Filme filme = new Filme("Filme 01", 1, 10.0);
-		
-		//Ação
-		Locacao locacao = service.alugarFilme(usuario, filme);
-		
-		//Verificação
-		Assertions.assertTrue(locacao.getUsuario().getNome() == "Usuário 01");
-		Assertions.assertTrue(DataUtils.boDatasIguais(locacao.getDataLocacao(), new Date()));
-		Assertions.assertTrue(DataUtils.boDatasIguais(locacao.getDataRetorno(), DataUtils.incrementarQntDias(1)));
-		
-	}
+  @Test
+  public void teste01() {
+    // Cenário
+    LocacaoService service = new LocacaoService();
+    Usuario usuario = new Usuario("Usuário 02");
+    Filme filme = new Filme("Filme 01", 1, 10.0);
+
+    // Ação
+    Locacao locacao = service.alugarFilme(usuario, filme);
+
+    // Verificação
+    Assertions.assertTrue(locacao.getUsuario().getNome() == "Usuário 01");
+    Assertions.assertTrue(DataUtils.boDatasIguais(locacao.getDataLocacao(), new Date()));
+    Assertions.assertTrue(DataUtils.boDatasIguais(locacao.getDataRetorno(), DataUtils.incrementarQntDias(1)));
+
+  }
 
 }
